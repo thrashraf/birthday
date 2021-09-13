0952fc4a9dbb4084ca3d215f32a30ad19f41747c
thrashraf <zulashrafvns@gmail.com>
2021-09-13 10:29:08 +0800

    first Try

---
+++ app.js
@@ -0,0 +1,74 @@
+
+//grab everything we need
+const buttons = document.querySelector('.btn-crazy');
+const normalBtn = document.querySelector('.normal-btn');
+
+
+
+//functions
+function goNuts(e) {
+
+    const button = e.target;
+    console.log(e.target);
+    const offshetWidth = Math.random() * (window.innerWidth - button.clientWidth);
+    const offshetHeight = Math.random() * (window.innerHeight - button.clientHeight);
+
+
+    button.style.top = `${offshetHeight}px`;
+    button.style.left = `${offshetWidth}px`;
+
+
+}
+
+//addEventListener
+buttons.addEventListener('mouseenter' , goNuts);
+normalBtn.addEventListener('click', () => {
+
+    window.location.href = 'index.html';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
